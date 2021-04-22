bceb3b6f3c04648982903402c4627056e10ed84f
SantiagoSE <>
2021-04-22 20:04:28 +0000

    Reto 4 completado (SM)

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,11 @@
+a = 18
+b = 30
+
+if (a<b):
+  print ('El numero ', a, 'es menor que ', b )
+
+if (a>b):
+  print('El numero ', a, 'es mayor que ', b )
+
+if(a==b):
+  print('El numero ', a, 'es igual que ', b )
