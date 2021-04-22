8cb1a96c29f701b580c945e8c1bd0a5cb027816a
SantiagoSE <>
2021-04-22 21:01:03 +0000

    Reto 14 terminado(SM)

---
+++ py2_retos1/reto14.py
@@ -0,0 +1,14 @@
+b = list(range(-10,11))
+
+def cuadn (a):
+  c = a*a
+  return c
+
+def terminado():
+  print('"Programa finalizado."')
+
+for i in range(-10,11):
+  nc = cuadn(i)
+  print(nc)
+
+terminado()
